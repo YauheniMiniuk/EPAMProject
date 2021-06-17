1d98fd8ae08cb5733526637798810113a442979c
Evgeny Minuk <minyk20000@gmail.com>
2021-06-17 16:48:06 +0300

    Done

---
+++ EPAMProject/EPAMProject/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace EPAMProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.selectJSONPathBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Путь к файлу";
+            // 
+            // openFileBtn
+            // 
+            this.openFileBtn.Location = new System.Drawing.Point(390, 25);
+            this.openFileBtn.Name = "openFileBtn";
+            this.openFileBtn.Size = new System.Drawing.Size(81, 23);
+            this.openFileBtn.TabIndex = 2;
+            this.openFileBtn.Text = "Открыть";
+            this.openFileBtn.UseVisualStyleBackColor = true;
+            this.openFileBtn.Click += new System.EventHandler(this.openFileBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Путь к файлу JSON";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(176, 94);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(106, 32);
+            this.calculateBtn.TabIndex = 5;
+            this.calculateBtn.Text = "Подсчитать";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // selectJSONPathBtn
+            // 
+            this.selectJSONPathBtn.Location = new System.Drawing.Point(390, 55);
+            this.selectJSONPathBtn.Name = "selectJSONPathBtn";
+            this.selectJSONPathBtn.Size = new System.Drawing.Size(81, 23);
+            this.selectJSONPathBtn.TabIndex = 6;
+            this.selectJSONPathBtn.Text = "Выбрать";
+            this.selectJSONPathBtn.UseVisualStyleBackColor = true;
+            this.selectJSONPathBtn.Click += new System.EventHandler(this.selectJSONPathBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 138);
+            this.Controls.Add(this.selectJSONPathBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.openFileBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "TextCalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openFileBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button selectJSONPathBtn;
+    }
+}
+
---
+++ EPAMProject/EPAMProject/Form1.cs
@@ -0,0 +1,163 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace EPAMProject
+{
+    public partial class Form1 : Form
+    {
+        public string filename, filenameNoType, longestWord;
+        public int fileSize, lettersCount, wordsCount, linesCount, digitsCount, numbersCount, wordsWithHyphen, punctuations;
+        Dictionary<char, int> letters = new Dictionary<char, int>();
+        Dictionary<string, int> words = new Dictionary<string, int>();
+        char[] separators = new char[] { ',', '.', '?', ';', ':', '"', '!', '\'' };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openFileBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = openFileDialog.FileName;
+                    filename = openFileDialog.SafeFileName;
+                    filenameNoType = Path.GetFileNameWithoutExtension(textBox1.Text);
+                }
+            }
+
+        }
+        private void selectJSONPathBtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog browserDialog = new FolderBrowserDialog())
+            {
+                if (browserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = browserDialog.SelectedPath;
+                }
+            }
+        }
+        string fileStr;
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader(textBox1.Text))
+            {
+                fileSize = 0;
+                lettersCount = 0;
+                letters.Clear();
+                wordsCount = 0;
+                words.Clear();
+                linesCount = 1;
+                digitsCount = 0;
+                numbersCount = 0;
+                longestWord = "";
+                wordsWithHyphen = 0;
+                punctuations = 0;
+
+                fileStr = sr.ReadToEnd();
+                fileSize = fileStr.Length;
+                foreach (char sym in fileStr)
+                {
+                    if (sym >= 'A' && sym <= 'z')
+                    {
+                        lettersCount++;
+                        if (letters.ContainsKey(sym))
+                        {
+                            letters[sym]++;
+                        }
+                        else
+                        {
+                            letters.Add(sym, 1);
+                        }
+                    }
+                    if (sym >= '0' && sym <= '9')
+                    {
+                        digitsCount++;
+                    }
+                    foreach (char punc in separators)
+                    {
+                        if (sym == punc)
+                        {
+                            punctuations++;
+                        }
+                    }
+                    if (sym == '\n')
+                    {
+                        linesCount++;
+                    }
+                }
+            }
+            // Remove punctuation marks
+            fileStr = Regex.Replace(fileStr, @"\s-\s", string.Empty);
+            fileStr = Regex.Replace(fileStr, @"\r\n", " ");
+            fileStr = Regex.Replace(fileStr, @"\d-\d", " ");
+            fileStr = Regex.Replace(fileStr, "[@,./?!_\\'\"—;:]", string.Empty);
+            string[] textArr = fileStr.Split(' ');
+            wordsCount = textArr.Length;
+            foreach (string str in textArr)
+            {
+                if ((str.Trim(separators)).Length > 0 && !Regex.IsMatch(str, @"^\d+$"))
+                {
+                    if (words.ContainsKey(str))
+                    {
+                        words[str]++;
+                    }
+                    else
+                    {
+                        words.Add(str, 1);
+                    }
+                }
+                longestWord = str.Length > longestWord.Length ? str : longestWord;
+                if (str.Contains("-"))
+                {
+                    wordsWithHyphen++;
+                }
+                if (int.TryParse(str, out _))
+                {
+                    numbersCount++;
+                }
+            }
+            letters = (from l in letters orderby l.Value descending select l).ToDictionary(p => p.Key, p => p.Value);
+            words = (from w in words orderby w.Value descending select w).ToDictionary(p => p.Key, p => p.Value);
+
+            // Create anon object
+            var jsonObj = new
+            {
+                filename = filename,
+                fileSize = fileSize,
+                lettersCount = lettersCount,
+                letters = letters,
+                wordsCount = wordsCount,
+                words = words,
+                linesCount = linesCount,
+                digitsCount = digitsCount,
+                numbersCount = numbersCount,
+                longestWord = longestWord,
+                wordsWithHyphen = wordsWithHyphen,
+                punctuations = punctuations
+            };
+            
+            var json = JsonConvert.SerializeObject(jsonObj, Formatting.Indented);
+            // Create .json file
+            string path = textBox2.Text + "/" + filenameNoType + ".json";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                if (File.Exists(path))
+                {
+                    sw.Write(json);
+                }
+                else
+                {
+                    File.Create(path);
+                    sw.Write(json);
+                }
+            }
+            MessageBox.Show("OK");
+        }
+    }
+}
